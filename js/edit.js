ee6bce9cc35f469b62771efa79eaa393a7d2991d
Marius <marius@example.com>
2022-08-31 11:46:05 +0300

    sendUpdatePostRequest

--- js/edit.js
+++ js/edit.js
@@ -23,7 +23,33 @@ getPost(`${baseUrl}/articles/${currentPostId}`);
 // supildyti formos laukus su posto info
 function fillFormWithData(postData, formToFill) {
   console.log('postData ===', postData);
+  formToFill.elements.author.value = postData.author;
+  formToFill.elements.body.value = postData.body;
+  formToFill.elements.category.value = postData.category;
 }
 // klausyti formos issiuntimo evento
 
+formEl.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const updatedPost = {
+    author: formEl.elements.author.value,
+    body: formEl.elements.body.value,
+    category: formEl.elements.category.value,
+  };
+  console.log('updatedPost ===', updatedPost);
+  const resp = await sendUpdatePostRequest(updatedPost);
+  console.log('resp ===', resp);
+  if (resp.ok) {
+    window.location.href = 'index.html';
+  }
+});
+
 // submitinant forma siusti put requesta i BE
+async function sendUpdatePostRequest(updatedPost) {
+  const resp = await fetch(`${baseUrl}/articles/${currentPostId}`, {
+    method: 'PUT',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(updatedPost),
+  });
+  return resp;
+}
