d2b37c8a65c0e8c7b29c920ed7c736ed30a577ce
Marius <marius@example.com>
2022-08-31 11:25:56 +0300

    fillFormWithData

--- js/edit.js
+++ js/edit.js
@@ -1,15 +1,29 @@
 'use strict';
 console.log('edit.js');
 
+const baseUrl = 'http://localhost:3000/api';
+
 // html sukurti 3 ivesties laukus, author, categorija ir body
 
 // js gauti query parametra
 const currentPostId = new URLSearchParams(window.location.search).get('postId');
 console.log('currentPostId ===', currentPostId);
+
+// Taikomes
+const formEl = document.forms[0];
+
 // parsisiusti su fetch posta su id is query params
-
+async function getPost(url) {
+  const resp = await fetch(url);
+  const dataInJs = await resp.json();
+  // console.log('dataInJs ===', dataInJs);
+  fillFormWithData(dataInJs, formEl);
+}
+getPost(`${baseUrl}/articles/${currentPostId}`);
 // supildyti formos laukus su posto info
-
+function fillFormWithData(postData, formToFill) {
+  console.log('postData ===', postData);
+}
 // klausyti formos issiuntimo evento
 
 // submitinant forma siusti put requesta i BE
