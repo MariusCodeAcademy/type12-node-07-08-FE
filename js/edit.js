157f03146c99c7e33fbad99a5ff803c75645e43e
Marius <marius@example.com>
2022-08-31 10:47:49 +0300

    edit post

---
+++ js/edit.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log('edit.js');
+
+// html sukurti 3 ivesties laukus, author, categorija ir body
+
+// js gauti query parametra
+
+// parsisiusti su fetch posta su id is query params
+
+// supildyti formos laukus su posto info
+
+// klausyti formos issiuntimo evento
+
+// submitinant forma siusti put requesta i BE
--- js/main.js
+++ js/main.js
@@ -18,7 +18,14 @@ function makePostsList(arr, dest) {
   dest.innerHtml = '';
   arr.forEach((pObj) => {
     const liEl = document.createElement('li');
+
     liEl.textContent = `${pObj.author}, cat: ${pObj.category}`;
+    // sukurti nuoroda editinimui
+    const editLinkEl = document.createElement('a');
+    editLinkEl.href = `edit.html?postId=${pObj.id}`;
+    editLinkEl.textContent = 'edit post';
+    editLinkEl.className = 'edit-link';
+    liEl.append(editLinkEl);
     dest.append(liEl);
   });
 }
