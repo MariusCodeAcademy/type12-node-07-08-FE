58f5024ba13730cb481a87be9f62c7749b12a00d
Marius <marius@example.com>
2022-08-31 10:59:55 +0300

    currentPostId

--- js/edit.js
+++ js/edit.js
@@ -4,7 +4,8 @@ console.log('edit.js');
 // html sukurti 3 ivesties laukus, author, categorija ir body
 
 // js gauti query parametra
-
+const currentPostId = new URLSearchParams(window.location.search).get('postId');
+console.log('currentPostId ===', currentPostId);
 // parsisiusti su fetch posta su id is query params
 
 // supildyti formos laukus su posto info
